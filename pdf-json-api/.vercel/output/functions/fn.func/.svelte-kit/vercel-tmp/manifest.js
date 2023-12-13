1e719caff4ad2f8bb82ac531601057b6e91ab719
lucas <lucasalves382@hotmail.com>
2023-12-13 14:04:12 -0300

    UPDATE: updating vercel adapter

---
+++ pdf-json-api/.vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -0,0 +1,34 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.e17c2b74.js","app":"_app/immutable/entry/app.d8c58f62.js","imports":["_app/immutable/entry/start.e17c2b74.js","_app/immutable/chunks/scheduler.02d168ce.js","_app/immutable/chunks/singletons.ce0d52e6.js","_app/immutable/chunks/index.3fb55e4e.js","_app/immutable/entry/app.d8c58f62.js","_app/immutable/chunks/scheduler.02d168ce.js","_app/immutable/chunks/index.2dc85c41.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			__memo(() => import('../output/server/nodes/0.js')),
+			__memo(() => import('../output/server/nodes/1.js')),
+			__memo(() => import('../output/server/nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+}
+})();
--- pdf-json-api/vite.config.ts
+++ pdf-json-api/vite.config.ts
@@ -1,10 +1,18 @@
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import purgeCss from 'vite-plugin-tailwind-purgecss'; // Importe corretamente o plugin
 
 export default defineConfig({
 	optimizeDeps: {
 		include: ['svelte', 'svelte-routing', 'skeletonlabs'],
 	},
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [
+		sveltekit(),
+		purgeCss({
+			// Configurações do purgeCss, como 'content' (conteúdo a ser verificado para remoção de CSS não utilizado)
+			// Consulte a documentação do plugin para as opções corretas de configuração
+			content: ['./src/**/*.svelte'],
+			// Outras opções...
+		}),
+	],
 });
