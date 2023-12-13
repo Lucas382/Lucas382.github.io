5c99a239091930c4d3f176aad0f6f8c33bddecc3
lucas <lucasalves382@hotmail.com>
2023-12-13 13:40:03 -0300

    HOTFIX: adding optimezeDeps

--- pdf-json-api/vite.config.ts
+++ pdf-json-api/vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	optimizeDeps: {
+		include: ['svelte', 'svelte-routing', 'skeletonlabs'],
+	},
 	plugins: [sveltekit(), purgeCss()]
 });
